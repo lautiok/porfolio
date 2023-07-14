ec35c908eb9738e2822a86b37672a4c4b93611fa
Nahuel Guerra <nahuelguerra56b@gmail.com>
2023-07-14 11:58:52 -0300

    escribit p

--- js/menu.js
+++ js/menu.js
@@ -4,3 +4,16 @@ var nav = document.querySelector('.nav');
 button.addEventListener('click', function () {
     nav.classList.toggle('activo');
 });
+const text = "Full Stack Developer studies at Coderhouse | Developer with extensive experience in independent projects.";
+let index = 0;
+let speed = 100;
+
+function typeWriter() {
+    if (index < text.length) {
+        document.getElementById("typewriter").innerHTML += text.charAt(index);
+        index++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+window.addEventListener('load', typeWriter);
