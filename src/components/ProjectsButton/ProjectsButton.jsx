88dd7ae2ea37c19f2360d5f364a9dfadd5a3017b
Nahuel Guerra <nahuelguerra56b@gmail.com>
2024-05-02 15:13:44 -0300

    ecommerce

--- src/components/ProjectsButton/ProjectsButton.jsx
+++ src/components/ProjectsButton/ProjectsButton.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import './ProjectsBStyle.css';
-import ipm from '../../assets/projects/ipm.png';
+import ecommerce from '../../assets/projects/ipm.png';
 import togo from '../../assets/projects/togo.png';
 import { useTranslation } from 'react-i18next';
 
@@ -37,14 +37,14 @@ export const ProjectsButton = () => {
     <div className='projects-button-container'>
       {/* Renderizar las tarjetas */}
       <div className='projects-button'>
-        <div className='img' onClick={() => handleCardClick('ipm')}>
-          <img src={ipm} alt='ipm' />
+        <div className='img' onClick={() => handleCardClick('ecommerce')}>
+          <img src={ecommerce} alt='ecommerce' />
         </div>
         <div className='info-project-button'>
-          <h2 onClick={() => handleCardClick('ipm')}>{t('projects.ipm.title')}</h2>
+          <h2 onClick={() => handleCardClick('ecommerce')}>{t('projects.ecommerce.title')}</h2>
           <div className='button-g-p'>
-            <a href='https://github.com/lautiok/Ipm' target='_blank'> <i className='bi bi-github'></i></a>
-            <a href='https://institutoprivadodemerlo.vercel.app/' target='_blank' > <i className='bi bi-link-45deg'></i></a>
+            <a href='https://github.com/lautiok/Ecommerce' target='_blank'> <i className='bi bi-github'></i></a>
+            <a href='#' target='_blank' > <i className='bi bi-link-45deg'></i></a>
           </div>
         </div>
       </div>
@@ -66,15 +66,15 @@ export const ProjectsButton = () => {
             <button className='close' onClick={handleCloseDetails}>Cerrar</button>
             <div className='modal-content'>
               <div className='img-modal'>
-              <img src={selectedProject === 'ipm' ? ipm : togo} alt={selectedProject === 'ipm' ? 'ipm' : 'togo'} />
+              <img src={selectedProject === 'ecommerce' ? ecommerce : togo} alt={selectedProject === 'ecommerce' ? 'ecommerce' : 'togo'} />
             </div>
             <div className='info-modal'>
-              <h2>{selectedProject === 'ipm' ? t('projects.ipm.title') : t('projects.togo.title')}</h2>
-              <p>{selectedProject === 'ipm' ? t('projects.ipm.technologies') : t('projects.togo.technologies')}</p>
-              <h3>{selectedProject === 'ipm' ? t('projects.ipm.description') : t('projects.togo.description')}</h3>
+              <h2>{selectedProject === 'ecommerce' ? t('projects.ecommerce.title') : t('projects.togo.title')}</h2>
+              <p>{selectedProject === 'ecommerce' ? t('projects.ecommerce.technologies') : t('projects.togo.technologies')}</p>
+              <h3>{selectedProject === 'ecommerce' ? t('projects.ecommerce.description') : t('projects.togo.description')}</h3>
             <div className='button-g-p-modal'>
-              <a href={selectedProject === 'ipm' ? 'https://github.com/lautiok/Ipm' : 'https://github.com/lautiok/togo'} target='_blank'> <i className='bi bi-github'></i></a>
-              <a href={selectedProject === 'ipm' ? 'https://institutoprivadodemerlo.vercel.app/' : 'https://togo-hazel.vercel.app/'} target='_blank' > <i className='bi bi-link-45deg'></i></a>
+              <a href={selectedProject === 'ecommerce' ? 'https://github.com/lautiok/Ecommerce' : 'https://github.com/lautiok/togo'} target='_blank'> <i className='bi bi-github'></i></a>
+              <a href={selectedProject === 'ecommerce' ? '#' : 'https://togo-hazel.vercel.app/'} target='_blank' > <i className='bi bi-link-45deg'></i></a>
             </div>
             </div>
             </div>
