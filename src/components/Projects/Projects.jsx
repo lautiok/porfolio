114098f8ff3911c10ed0f5d1c7efa2b7ac4def49
Nahuel Guerra <nahuelguerra56b@gmail.com>
2024-06-09 12:57:28 -0300

    new: project-sp

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,6 +3,8 @@ import './ProjectsModule.css'
 import { useTranslation } from 'react-i18next'
 import coffe from '../../assets/projects/coffe.png'
 import eco from '../../assets/projects/wolf.png'
+import tienda from '../../assets/projects/eco.png'
+import sp from '../../assets/projects/sp.png'
 export const Projects = () => {
     const [t] = useTranslation('global')
 
@@ -10,6 +12,34 @@ export const Projects = () => {
         <div id='projects' className='project-container'>
             <div id='project' className='projects'><h2> <i className="bi bi-backpack4-fill"></i> {t('projects.title')}</h2></div>
             <div className='projects'>
+            <div className='project-module'>
+                    <div className='project-img'>
+                        <img src={sp} alt="projects" />
+                    </div>
+                    <div className='project-description'>
+                        <h2>{t('projects.sp.title')}</h2>
+                        <p>{t('projects.sp.technologies')}</p>
+                        <p className='description'>{t('projects.sp.description')}</p>
+                        <div className='button-project'>
+                            <a href="https://github.com/lautiok/Adoption-web-application" target="_blank" rel="noreferrer" ><i className="bi bi-github"></i> {t('projects.button.git')}</a>
+                            <a href="https://adoption-web-application.vercel.app/" target="_blank" rel="noreferrer" ><i className="bi bi-link-45deg"></i> {t('projects.button.live')}</a>
+
+                        </div>
+                    </div>
+                </div>
+            <div className='project-module'>
+                    <div className='project-img'>
+                        <img src={tienda} alt="projects" />
+                    </div>
+                    <div className='project-description'>
+                        <h2>{t('projects.ecommerce.title')}</h2>
+                        <p>{t('projects.ecommerce.technologies')}</p>
+                        <p className='description'>{t('projects.ecommerce.description')}</p>
+                        <div className='button-project'>
+                            <a href="https://github.com/lautiok/Ecommerce" target="_blank" rel="noreferrer" ><i className="bi bi-github"></i> {t('projects.button.git')}</a>
+                        </div>
+                    </div>
+                </div>
                 <div className='project-module'>
                     <div className='project-img'>
                         <img src={coffe} alt="projects" />
