93f32f1eb06c40b2110ce24a6596fa69ef3d605b
“Nahuel <nahuelguerra56b@gmail.com>
2025-07-05 14:08:06 -0300

    refactor: remove headers and redirects configuration from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,30 +3,8 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Encoding",
-          },
-        ],
-      },
-    ];
-  },
-  async redirects() {
-    return [
-      {
-        source: "/old-route",
-        destination: "/new-route",
-        permanent: true,
-      },
-    ];
-  },
-  poweredByHeader: false,
-  compress: true, 
-};
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
+
+
