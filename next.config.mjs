20132dbee4e39e6b9e6bb1b6e798294dba2cc86d
“Nahuel <nahuelguerra56b@gmail.com>
2025-07-05 14:06:58 -0300

    feat: configure headers and redirects in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,30 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Encoding",
+          },
+        ],
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: "/old-route",
+        destination: "/new-route",
+        permanent: true,
+      },
+    ];
+  },
+  poweredByHeader: false,
+  compress: true, 
+};
 
 export default withNextIntl(nextConfig);
